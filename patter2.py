f656d6801b38d9cc7f9c445eb09a0e2df016fefa
Suryansh Tripathi <tripathisuryansh800@gmail.com>
2025-06-27 18:20:04 +0530

    ..

---
+++ patter2.py
@@ -0,0 +1,18 @@
+from turtle import*
+#heart
+bgcolor('light blue')
+speed('slowest')
+fillcolor('red')
+begin_fill()
+left(140)
+circle(-90,200)
+left(120)
+circle(-90,200)
+forward(180)
+end_fill()
+hideturtle()
+
+
+mainloop()
+
+
---
+++ pattern.py
@@ -0,0 +1,15 @@
+from turtle import*
+#heart
+pencolor("red")
+speed('slowest')
+fillcolor("red")
+begin_fill()
+left(140)
+circle(-90,200)
+left(120)
+circle(-90,200)
+forward(180)
+end_fill()
+hideturtle()
+
+mainloop()
